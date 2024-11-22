3c5a9123314232b4deea5536d09f1fdf0f1c86f4
David <oppongnkrumahdavid@gmail.com>
2024-11-22 15:08:08 +0000

    excercises 1.11* - 11.14*

--- part1/src/App.jsx
+++ part1/src/App.jsx
@@ -1,69 +1,76 @@
 import { useState } from "react";
 
-const Button = ({ handleCount, text }) => {
-  const buttonName = text;
-  return <button onClick={handleCount}>{buttonName}</button>;
+const indexOfMax = (arr) => {
+  if (arr.length == 0) {
+    return -1;
+  }
+  var max = arr[0];
+  var maxIndex = 0;
+
+  for (let i = 1; i < arr.length; i++) {
+    if (arr[i] > max) {
+      maxIndex = i;
+      max = arr[i];
+    }
+  }
+  return maxIndex;
 };
 
-const StatisticLine = ({ value, text }) => {
+const Button = ({ text, handleFunc }) => {
   return (
-    <tr>
-      <td>{text}</td>
-      <td>
-        {" "}
-        {value} {text == "positive" && "%"}
-      </td>
-    </tr>
-  );
-};
-const Statistics = ({ good, neutral, bad, count, all }) => {
-  return (
-    <table>
-      <StatisticLine text="good" value={good} />
-      <StatisticLine text="neutral" value={neutral} />
-      <StatisticLine text="bad" value={bad} />
-      <StatisticLine text="all" value={all} />
-      <StatisticLine
-        text="average"
-        value={
-          (count.good * good + count.neutral * neutral + count.bad * bad) / all
-        }
-      />
-      <StatisticLine text="positive" value={(good / all) * 100} />
-    </table>
+    <>
+      <button onClick={handleFunc}>{text}</button>
+    </>
   );
 };
 
 const App = () => {
-  // save clicks of each button to its own state
-  const [good, setGood] = useState(0);
-  const [neutral, setNeutral] = useState(0);
-  const [bad, setBad] = useState(0);
+  const anecdotes = [
+    "If it hurts, do it more often.",
+    "Adding manpower to a late software project makes it later!",
+    "The first 90 percent of the code accounts for the first 90 percent of the development time...The remaining 10 percent of the code accounts for the other 90 percent of the development time.",
+    "Any fool can write code that a computer can understand. Good programmers write code that humans can understand.",
+    "Premature optimization is the root of all evil.",
+    "Debugging is twice as hard as writing the code in the first place. Therefore, if you write the code as cleverly as possible, you are, by definition, not smart enough to debug it.",
+    "Programming without an extremely heavy use of console.log is same as if a doctor would refuse to use x-rays or blood tests when diagnosing patients.",
+    "The only way to go fast, is to go well.",
+  ];
 
-  const all = good + neutral + bad;
-  const count = { good: 1, neutral: 0, bad: -1 };
+  const [selected, setSelected] = useState(0);
+  const [voted, setVoted] = useState(Array(anecdotes.length).fill(0));
 
   return (
-    <div>
-      <h1>give feedback</h1>
-      <br />
-      <Button handleCount={() => setGood(good + 1)} text="good" />
-      <Button handleCount={() => setNeutral(neutral + 1)} text="neutral" />
-      <Button handleCount={() => setBad(bad + 1)} text="bad" />
-      <br />
-      <h1>statistics</h1>
-      {good || neutral || bad ? (
-        <Statistics
-          good={good}
-          neutral={neutral}
-          bad={bad}
-          count={count}
-          all={all}
+    <>
+      <h1>Anecdote of the day</h1>
+      {anecdotes[selected]}
+      <div>has {voted[selected]} votes</div>
+      <div>
+        <Button
+          text="vote"
+          handleFunc={() =>
+            setVoted(
+              voted.map((value, i) => {
+                if (i == selected) {
+                  return value + 1;
+                }
+                return value;
+              })
+            )
+          }
         />
-      ) : (
-        "No feedback given"
-      )}
-    </div>
+        <Button
+          text="nextAnecdote"
+          handleFunc={() =>
+            setSelected(Math.floor(Math.random() * anecdotes.length))
+          }
+          arrayLength={anecdotes.length}
+        />
+      </div>
+      <div>
+        <h1>Anecdote with most votes</h1>
+        <div>{anecdotes[indexOfMax(voted)]}</div>
+      </div>
+    </>
   );
 };
 
